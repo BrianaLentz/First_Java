578ff6d2ddabf281e382897043ed8a9c631ca2b6
BrianaLentz <98199391+BrianaLentz@users.noreply.github.com>
2022-04-04 13:32:43 -0700

    first java program

---
+++ FirstJava.java
@@ -0,0 +1,14 @@
+public class FirstJava{
+    public static void main(String[] args){
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+    }
+    String name = "Briana V. Lentz";
+    int age = 30;
+    String hometown = "Los Angeles then Reno, Nv";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(hometown);
+    }
+}
